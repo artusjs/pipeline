2f4f8adbd936af7a42d6d7ef5df9202bcb912072
hyj1991 <yeekwanvong@gmail.com>
2022-03-09 15:39:28 +0800

    use es2017

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -2,17 +2,12 @@
 import { Middlewares, MiddlewareInput, BaseContext } from "./types";
 
 export class Pipeline {
-  private readonly middlewares: Middlewares;
-  private index: number;
-
-  constructor() {
-    this.middlewares = [];
-    this.index = -1;
-  }
+  #middlewares: Middlewares = [];
+  #index: number =  -1;
 
   use(middleware: MiddlewareInput) {
     if (typeof middleware === 'function') {
-      this.middlewares.push(middleware);
+      this.#middlewares.push(middleware);
       return;
     }
 
@@ -27,19 +22,19 @@ export class Pipeline {
     }
   }
 
-  private dispatch(i: number, ctx: BaseContext<any>): Promise<any> {
-    if (i <= this.index) return Promise.reject(new Error('next() called multiple times'));
-    this.index = i;
-    let fn = this.middlewares[i];
+  #dispatch(i: number, ctx: BaseContext<any>): Promise<any> {
+    if (i <= this.#index) return Promise.reject(new Error('next() called multiple times'));
+    this.#index = i;
+    let fn = this.#middlewares[i];
     if (!fn) return Promise.resolve();
     try {
-      return Promise.resolve(fn(ctx, this.dispatch.bind(this, i + 1, ctx)));
+      return Promise.resolve(fn(ctx, this.#dispatch.bind(this, i + 1, ctx)));
     } catch (err: any) {
       return Promise.reject(err);
     }
   }
 
   run(ctx: BaseContext<any>) {
-    return this.dispatch(0, ctx);
+    return this.#dispatch(0, ctx);
   }
 }
